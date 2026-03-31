86e2614f9ac4aaa80c6b59c3526ae741c611b7ab
Parker2107 <113683584+Parker2107@users.noreply.github.com>
2026-04-01 03:26:02 +0530

    Project initialisation

---
+++ yt_ai_app/api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('your-endpoint/', views.your_view_function, name='your-endpoint'),
+]
---
+++ yt_ai_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class YtAiAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'yt_ai_app'
