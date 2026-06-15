207eb83031cceb97da23c96cc0e214b74a914fca
Parker2107 <jeevesh2107@gmail.com>
2026-06-15 19:18:29 +0530

    added cors for testing security and auth

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'yt_ai_app',
     'rest_framework',
     'django.contrib.admin',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -75,6 +77,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
--- yt_ai_app/api/urls.py
+++ yt_ai_app/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     # path('your-endpoint/', views.your_view_function, name='your-endpoint'),
+    path('health/', views.health_check, name='health-check')
 ]
